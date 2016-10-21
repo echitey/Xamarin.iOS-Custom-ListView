de5e6abfae386069c9a74febdbf063f7a077ad71
Yawo Echitey <yawo@echitey.com>
2016-10-21 16:58:59 -0200

    Added TableSource and TableView with Default Cell

---
+++ iOSCustomTableView/MyTableSource.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace iOSCustomTableView
+{
+	public class MyTableSource : UITableViewSource
+	{
+		List<Character> characters;
+		public MyTableSource(List<Character> _characters)
+		{
+			characters = _characters;
+		}
+
+		public override nint RowsInSection(UITableView tableview, nint section)
+		{
+			return characters.Count;
+		}
+
+		public override UITableViewCell GetCell(UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			var cell = tableView.DequeueReusableCell("characterCell");
+			cell.TextLabel.Text = characters[indexPath.Row].ToString();
+
+			return cell;
+		}
+
+		public override nfloat GetHeightForRow(UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			return 100f;
+		}
+	}
+
+	public class Character
+	{
+		public string name { get; set;}
+		public int age { get; set;}
+		public string picture { get; set;}
+		public override string ToString()
+		{
+			return string.Format("[Character: name={0}, age={1}, picture={2}]", name, age, picture);
+		}
+	}
+}
---
+++ iOSCustomTableView/ViewController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace iOSCustomTableView
+{
+	public partial class ViewController : UIViewController
+	{
+		private List<Character> characters;
+
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			characters = getCharacters();
+
+			characterTable.Source = new MyTableSource(characters);
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		private List<Character> getCharacters()
+		{
+			
+			var _characters = new List<Character>();
+
+			_characters.Add(new Character { name = "Goomba", age = 12, picture = "" });
+			_characters.Add(new Character { name = "Bowser", age = 23, picture = "" });
+			_characters.Add(new Character { name = "Mario", age = 34, picture = "" });
+			_characters.Add(new Character { name = "Racoon Mario", age = 35, picture = "" });
+			_characters.Add(new Character { name = "Shyguy Green", age = 11, picture = "" });
+			_characters.Add(new Character { name = "MArio Baby", age = 12, picture = "" });
+			_characters.Add(new Character { name = "Yoshi", age = 23, picture = "" });
+
+			return _characters;
+
+		}
+	}
+}
